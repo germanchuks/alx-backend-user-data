d5fb8d858838e048c4aa59e31fbfa26ce2c09eee
root <root@G3RM4N>
2024-06-03 15:51:27 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
 """ Module for Basic Authentication
 """
-from flask import request
-from typing import List, TypeVar
+import base64
 
 
 class BasicAuth:
     """ Basic Authentication class that inherits from Auth
     """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """ Returns Base64 part of the Authorization header for Basic Auth"""
-        if authorization_header is None or not isinstance(authorization_header, str):
+    def extract_base64_authorization_header(self, authorization_header:
+                                            str) -> str:
+        """ Returns Base64 part of the Authorization header for Basic Auth
+        """
+        if authorization_header is None or not \
+                isinstance(authorization_header, str):
             return None
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """ Returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None or not \
+                isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except:
+            return None
